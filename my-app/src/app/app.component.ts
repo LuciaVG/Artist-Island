cfed9f3145ddb5cb117723855ed15fc66bbb6c27
LuciaVG <bvluly@gmail.com>
2018-04-23 17:02:02 -0500

    landing page

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Cosa } from './cosa';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'Artist Island';
+  featured = [new Cosa('javier', 'kajsfkjdbshbv j sjb cwhbfi wufw jf wjhbfjweh efjh ', require('../images/camera.png')), new Cosa('grecia', 'kajsfkjdbshbv j sjb cwhbfi wufw jf wjhbfjweh efjh ', require('../images/camera.png')),new Cosa('pedro', 'kajsfkjdbshbv j sjb cwhbfi wufw jf wjhbfjweh efjh ', require('../images/camera.png'))];
 }
---
+++ my-app/src/app/cosa.ts
@@ -0,0 +1,6 @@
+export class Cosa {
+  constructor(
+    public name: string,
+    public description: string,
+    public url: string) { }
+}
